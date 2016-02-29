0fd56c88d78d128049a29b2f5dbe90f0104c42ad
ipc103 <iancandysemail@gmail.com>
2016-02-29 13:45:51 -0500

    add a test for phone number

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,12 +1,21 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
-	it('should add a todo', function() {
+describe('angularjs contact list', function() {
+	it('should filter the list by name', function() {
 		browser.get('http://localhost:8080');
 
 		element(by.model('ctrl.search')).sendKeys('Bob');
 
-		var todoList = element.all(by.repeater('contact in ctrl.contacts'));
-		expect(todoList.count()).toEqual(1);
+		var contacts = element.all(by.repeater('contact in ctrl.contacts'));
+		expect(contacts.count()).toEqual(1);
+	});
+
+	it('should filter the list by phone number', function() {
+		browser.get('http://localhost:8080');
+
+		element(by.model('ctrl.search')).sendKeys('015242 99250');
+
+		var contacts = element.all(by.repeater('contact in ctrl.contacts'));
+		expect(contacts.count()).toEqual(1);
 	});
 });
