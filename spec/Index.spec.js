51ed27c763b67c13997a7e68028309783e4c341b
Todd Motto <todd@toddmotto.com>
2016-03-02 12:48:42 +0000

    chore: Added date test

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,14 +1,26 @@
 var path = require('path');
 
-describe('angularjs contact list', function() {
+describe('Filters Test', function() {
 	it('should filter the list by name', function() {
 		browser.get('http://localhost:8080');
 
 		element(by.model('ctrl.search')).sendKeys('Bob');
 
-		var contacts = element.all(by.repeater('contact in ctrl.contacts'));
-		expect(contacts.count()).toEqual(1);
+		var todoList = element.all(by.repeater('contact in ctrl.contacts'));
+		expect(todoList.count()).toEqual(1);
 	});
+
+	it('should format the date correctly ', function() {
+		browser.get('http://localhost:8080');
+
+		element(by.model('ctrl.search')).sendKeys('Bob');
+
+		var todoList = element.all(by.repeater('contact in ctrl.contacts'));
+		var item = todoList.get(0).element(by.css('td:nth-child(6)'));
+
+		expect(item.getInnerHtml()).toBe('Tuesday, January 13, 1970');
+	});
+
 
 	it('should filter the list by phone number', function() {
 		browser.get('http://localhost:8080');
