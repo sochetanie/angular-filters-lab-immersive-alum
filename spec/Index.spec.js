716192a7d3b24b39663d70f6dd9605f861aa4673
Todd Motto <todd@toddmotto.com>
2016-03-02 12:46:47 +0000

    chore: Added date test

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,12 +1,16 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
-	it('should add a todo', function() {
+describe('Filters Test', function() {
+	it('should filter the repeat from the input', function() {
 		browser.get('http://localhost:8080');
 
 		element(by.model('ctrl.search')).sendKeys('Bob');
 
 		var todoList = element.all(by.repeater('contact in ctrl.contacts'));
 		expect(todoList.count()).toEqual(1);
+
+		var item = todoList.get(0).element(by.css('td:nth-child(6)'));
+
+		expect(item.getInnerHtml()).toBe('Tuesday, January 13, 1970');
 	});
 });
